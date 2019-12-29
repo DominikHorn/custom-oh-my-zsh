fbb390828e61306d64f300ed1442c836bddf95fe
Dominik Horn <dominik.horn@tum.de>
2019-12-29 15:40:46 +0100

    Add installation script for installing themes

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+NO_COLOR="\033[0m"
+OH_MY_ZSH_DIR=~/.oh-my-zsh
+CUSTOM_DIR=~/.custom-oh-my-zsh
+ZSHRC=~/.zshrc
+ZSHRC_BACKUP=~/.zshrc-backup-pre-custom-oh-my-zsh
+CUSTOMRC=~/.customrc
+
+# Install oh my zsh if not present
+if [[ ! -d $OH_MY_ZSH_DIR ]];
+then
+  print $YELLOW "Installing oh-my-zsh on system" $NO_COLOR
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Clone repo if not present (i.e., install script downloaded and executed separatelly)
+if [[ ! -d "$CUSTOM_DIR" ]];
+then
+  print $YELLOW "Cloning repository since no copy was found locally" $NO_COLOR
+  git clone https://github.com/DominikHorn/custom-oh-my-zsh.git $CUSTOM_DIR
+fi
+
+# Replace .zshrc with custom one
+if [[ -e "$ZSHRC" && ! -e "$ZSHRC_BACKUP" ]];
+then
+  # Backup old .zshrc
+  if [[ ! -e "$ZSHRC_BACKUP" ]];
+  then
+    print $RED "Replacing '$ZSHRC' Old file may be found under $ZSHRC_BACKUP" $NO_COLOR
+    mv $ZSHRC $ZSHRC_BACKUP
+  fi
+
+  # Install custom .zshrc
+  ln $CUSTOM_DIR/zshrc "$ZSHRC"
+  print $RED "Installed custom $ZSHRC. Please add applicable custom configuration to $CUSTOMRC" $NO_COLOR
+fi
+
+# Link all themes into the themes folder
+find $CUSTOM_DIR/themes/*.zsh-theme -exec ln -f {} $OH_MY_ZSH_DIR/themes \;
+print $GREEN "Linked all themes. You may now set one of the custom themes by editing the $ZSHRC file" $NO_COLOR
---
+++ zshrc
@@ -0,0 +1,37 @@
+# ZSH global settings
+export ZSH=~/.oh-my-zsh
+export UPDATE_ZSH_DAYS=5
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS=true
+
+# ZSH settings
+ENABLE_CORRECTION="false"
+COMPLETION_WAITING_DOTS="false"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="memes"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Which plugins would you like to load?
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+## Add ssh keys (-K ensures that passwords are loaded from keychain)
+ssh-add -K ~/.ssh/id_rsa
+
+## Useful aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ll='ls -alh'
+
+## Load custom configuration
+source ~/.customrc
